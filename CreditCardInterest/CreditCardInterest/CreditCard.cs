d8d08a9900893e6a86e8a33ee650c9fc4819c35e
Eric Ray <erichamion@gmail.com>
2016-09-19 18:25:46 -0500

    Implemented CreditCard

---
+++ CreditCardInterest/CreditCardInterest/CreditCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardInterest
+{
+    public class CreditCard
+    {
+        private IDiscreteInterestCalculator _interestCalculator;
+        private double principalIn;
+        private readonly double _rate;
+        private PrincipalInterestBalance _principalInterestBalance;
+
+        public string Name { get; }
+        public double Balance { get { return _principalInterestBalance.Total; } }
+
+        public double Principal { get { return _principalInterestBalance.Principal; } }
+        public double Interest { get { return _principalInterestBalance.Interest; } }
+        
+        public CreditCard(string name, double startingBalance, double ratePerPeriod, IDiscreteInterestCalculator interestCalculator)
+        {
+            Name = name;
+            _principalInterestBalance = new PrincipalInterestBalance { Principal = startingBalance, Interest = 0 };
+            _rate = ratePerPeriod;
+            _interestCalculator = interestCalculator;
+        }
+
+        
+        public void PassTime(int periods)
+        {
+            _principalInterestBalance = _interestCalculator.CalculateInterest(Principal, _rate, periods);
+        }
+    }
+}
--- CreditCardInterest/CreditCardInterestTests/CreditCardTests.cs
+++ CreditCardInterest/CreditCardInterestTests/CreditCardTests.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace CreditCardInterest.Tests
 {
     [TestClass]
     public class CreditCardTests
     {
-        //[TestMethod]
-        //public void TestMethod1()
-        //{
-        //}
+        [TestMethod]
+        public void CreditCard_PassTimeUsesInterestCalculator()
+        {
+            // Arrange
+            var principalIn = 3.0;
+            var rateIn = 0.1;
+            var periods = 42;
+            var principalOut = 27.0;
+            var interestOut = 42.0;
+            var expected = principalOut + interestOut;
+            var mockInterestCalculator = new Mock<IDiscreteInterestCalculator>();
+            mockInterestCalculator.Setup(x => x.CalculateInterest(principalIn, rateIn, periods)).Returns(new PrincipalInterestBalance { Principal = principalOut, Interest = interestOut });
+            var target = new CreditCard("foo", principalIn, 0.1, mockInterestCalculator.Object);
+            double actual;
+
+            // Act
+            target.PassTime(periods);
+            actual = target.Balance;
+
+            // Assert
+            mockInterestCalculator.Verify(x => x.CalculateInterest(principalIn, rateIn, periods), Times.Once);
+            Assert.AreEqual(expected, actual, 0.0000001);
+        }
     }
 }
