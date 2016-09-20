c7726a66445dfa955f9a60fce470c4088c07c6cf
Eric Ray <erichamion@gmail.com>
2016-09-19 19:22:08 -0500

    Implemented Wallet

---
+++ CreditCardInterest/CreditCardInterest/Wallet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardInterest
+{
+    public class Wallet
+    {
+        private readonly List<ITimeSensitiveAccount> _cards;
+
+        public Wallet(List<ITimeSensitiveAccount> cards)
+        {
+            _cards = cards;
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return _cards.Sum(x => x.Balance);
+            }
+        }
+
+        public void PassTime(int periods)
+        {
+            _cards.ForEach(x => x.PassTime(periods));
+        }
+    }
+}
---
+++ CreditCardInterest/CreditCardInterestTests/WalletTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CreditCardInterest.Tests
+{
+    [TestClass]
+    public class WalletTests
+    {
+        [TestMethod]
+        public void Wallet_PassTimeCallsAllCardPassTimes()
+        {
+            // Arrange
+            var periods = 27;
+            var mockData = new List<Mock<ITimeSensitiveAccount>>
+            {
+                new Mock<ITimeSensitiveAccount>(),
+                new Mock<ITimeSensitiveAccount>(),
+                new Mock<ITimeSensitiveAccount>(),
+            };
+            mockData.ForEach(x => x.Setup(y => y.PassTime(periods)));
+            var data = mockData.Select(x => x.Object).ToList();
+            var target = new Wallet(data);
+
+            // Act
+            target.PassTime(periods);
+
+            // Assert
+            mockData.ForEach(x => x.VerifyAll());
+        }
+
+        [TestMethod]
+        public void Wallet_BalanceSumsCardBalances()
+        {
+            // Arrange
+            var mockData = new List<Mock<ITimeSensitiveAccount>>
+            {
+                new Mock<ITimeSensitiveAccount>(),
+                new Mock<ITimeSensitiveAccount>(),
+                new Mock<ITimeSensitiveAccount>(),
+            };
+            var individualBalance = 27.0;
+            var expected = individualBalance * mockData.Count;
+            mockData.ForEach(x => x.Setup(y => y.Balance).Returns(individualBalance));
+            var data = mockData.Select(x => x.Object).ToList();
+            var target = new Wallet(data);
+            double actual;
+
+            // Act
+            actual = target.Balance;
+
+            // Assert
+            mockData.ForEach(x => x.VerifyAll());
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
